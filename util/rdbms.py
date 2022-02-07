fd0ef02be82bee7543a6cf65e21794be203ae75b
jeff <jeff@innotech.me>
2022-02-07 18:29:43 +0800

    update rdbms

--- util/rdbms.py
+++ util/rdbms.py
@@ -1,2 +1,8 @@
 import MySQLdb
+from dbutils.pooled_db import PooledDB, SharedDBConnection
+from MySQLdb.cursors import DictCursor
+
+pool = PooledDB(creator=MySQLdb, mincached=1, maxcached=5, host='127.0.0.1', user='root', password='4qwer',
+                db='rbmq', port=3306, cursorclass=DictCursor)
+
 
