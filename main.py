05ae15ad9c125651e14f8fc15554d1e65fb97f0e
jeff <jeff@innotech.me>
2022-04-17 16:48:05 +0800

    success get event loop when sanic app start

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
+import asyncio
 from dotenv import load_dotenv
-from sanic import Sanic, json
+from sanic import Sanic, json, app
 import os
-
 from exception.logic_error_exception import LogicErrorException
 from exception.unauthorized_exception import UnAuthorizedException
 
@@ -15,6 +15,17 @@ def create_app():
     app.error_handler.add(UnAuthorizedException, unauthorized_access)
     app.error_handler.add(LogicErrorException, catch_logic_error)
     app.error_handler.add(Exception, catch_anything)
+    # app.add_task(notify_server_started_after_five_seconds())
+
+    @app.listener('before_server_start')
+    def init(sanic, loop):
+        if loop is None:
+            print('loop is none')
+        print('before start')
+        # global sem
+        # concurrency_per_worker = 4
+        # sem = asyncio.Semaphore(concurrency_per_worker, loop=loop)
+
     return app
 
 
@@ -33,8 +44,19 @@ def catch_logic_error(request, e):
     return json({"code": e.code, "msg": e.msg}, status=500)
 
 
+async def notify_server_started_after_five_seconds():
+    await asyncio.sleep(5)
+    print('Server successfully started!')
+    loop = asyncio.get_running_loop()
+    print('loop get !')
+
+
+# @app.listener('before_server_start')
+# def init(sanic, loop):
+#     print('before start')
+
+
 if __name__ == '__main__':
     load_dotenv()
     create_app().run(host='127.0.0.1', port=os.getenv("SERVER_PORT"))
-
 
--- service/orders_service.py
+++ service/orders_service.py
@@ -1,67 +1,67 @@
-from decimal import Decimal
-from typing import Union
-
-from exception.logic_error_exception import LogicErrorException
-from model.token_body import TokenBody
-from util import rdbms
-from util.common import response_ok, response_error
-from util.rdbms import transaction
-from sql import product_sql, user_sql, order_sql
-
-
-@transaction
-async def create_order(items: list, token_body: TokenBody, conn):
-    # token_body = request.ctx.token_body
-    holder = []
-    ids = []
-    for item in items:
-        ids.append(item.productId)
-        holder.append('%s')
-    s = ','.join(holder)
-    dynamic_script = product_sql.SELECT_PRODUCT_FOR_UPDATE.format(s)
-    query_result = await rdbms.db.query_once(dynamic_script, ids)
-    bill_amount = 0
-    for item in items:
-        for product in query_result:
-            if item.productId != product['id']:
-                continue
-            if Decimal(item.price) != product['price']:
-                raise LogicErrorException(code=41, msg='price is not match. id: {}'.format(product['id']))
-            left_quantity = product['quantity'] - item.quantity
-            if left_quantity < 0:
-                raise LogicErrorException(4, 'product id: {}, insufficient quantity'.format(item.productId))
-
-            await rdbms.execute(sql=product_sql.UPDATE_PRODUCT_FOR_QUANTITY,
-                                params=[left_quantity, item.productId],
-                                conn=conn)
-            bill_amount = bill_amount + item.quantity*product['price']
-            break
-
-    update_row = await rdbms.execute(sql=user_sql.UPDATE_USER_BALANCE_ON_PURCHASE,
-                                     params=[bill_amount, token_body.id, bill_amount],
-                                     conn=conn)
-    if update_row == 0:
-        raise LogicErrorException(5, 'insufficient balance')
-
-    last_insert_order_id = await rdbms.insert_and_get_last_id(sql=order_sql.INSERT_ORDER_MAIN,
-                                                              params=[token_body.id, bill_amount, 1],
-                                                              conn=conn)
-
-    data = []
-    for item in items:
-        data.append((last_insert_order_id, item.productId, item.price, item.quantity))
-
-    insert_rows = await rdbms.batch_insert(order_sql.INSERT_ORDER_DETAIL, data, conn)
-    return response_ok(data={'billAmount': bill_amount, 'orderNo': last_insert_order_id})
-
-
-async def select_products(ids: Union[list, tuple]):
-    data = [1, 3, 7]
-    val = []
-    for i in data:
-        val.append('%s')
-
-    s = ','.join(val)
-    dynamic_script = product_sql.GET_PRODUCT_BY_IDS.format(s)
-    query_data = await rdbms.db.query_once(dynamic_script, data)
-    return response_ok(data=query_data)
+# from decimal import Decimal
+# from typing import Union
+#
+# from exception.logic_error_exception import LogicErrorException
+# from model.token_body import TokenBody
+# from util import rdbms
+# from util.common import response_ok, response_error
+# from util.rdbms import transaction
+# from sql import product_sql, user_sql, order_sql
+#
+#
+# @transaction
+# async def create_order(items: list, token_body: TokenBody, conn):
+#     # token_body = request.ctx.token_body
+#     holder = []
+#     ids = []
+#     for item in items:
+#         ids.append(item.productId)
+#         holder.append('%s')
+#     s = ','.join(holder)
+#     dynamic_script = product_sql.SELECT_PRODUCT_FOR_UPDATE.format(s)
+#     query_result = await rdbms.db.query_once(dynamic_script, ids)
+#     bill_amount = 0
+#     for item in items:
+#         for product in query_result:
+#             if item.productId != product['id']:
+#                 continue
+#             if Decimal(item.price) != product['price']:
+#                 raise LogicErrorException(code=41, msg='price is not match. id: {}'.format(product['id']))
+#             left_quantity = product['quantity'] - item.quantity
+#             if left_quantity < 0:
+#                 raise LogicErrorException(4, 'product id: {}, insufficient quantity'.format(item.productId))
+#
+#             await rdbms.execute(sql=product_sql.UPDATE_PRODUCT_FOR_QUANTITY,
+#                                 params=[left_quantity, item.productId],
+#                                 conn=conn)
+#             bill_amount = bill_amount + item.quantity*product['price']
+#             break
+#
+#     update_row = await rdbms.execute(sql=user_sql.UPDATE_USER_BALANCE_ON_PURCHASE,
+#                                      params=[bill_amount, token_body.id, bill_amount],
+#                                      conn=conn)
+#     if update_row == 0:
+#         raise LogicErrorException(5, 'insufficient balance')
+#
+#     last_insert_order_id = await rdbms.insert_and_get_last_id(sql=order_sql.INSERT_ORDER_MAIN,
+#                                                               params=[token_body.id, bill_amount, 1],
+#                                                               conn=conn)
+#
+#     data = []
+#     for item in items:
+#         data.append((last_insert_order_id, item.productId, item.price, item.quantity))
+#
+#     insert_rows = await rdbms.batch_insert(order_sql.INSERT_ORDER_DETAIL, data, conn)
+#     return response_ok(data={'billAmount': bill_amount, 'orderNo': last_insert_order_id})
+#
+#
+# async def select_products(ids: Union[list, tuple]):
+#     data = [1, 3, 7]
+#     val = []
+#     for i in data:
+#         val.append('%s')
+#
+#     s = ','.join(val)
+#     dynamic_script = product_sql.GET_PRODUCT_BY_IDS.format(s)
+#     query_data = await rdbms.db.query_once(dynamic_script, data)
+#     return response_ok(data=query_data)
